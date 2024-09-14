f84057893d1ab03f4236c69eb7ba794494e48dd0
Raus <rausaray1234@gmail.com>
2024-09-14 18:49:16 +0545

    CORS Policy Fixed

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,13 +2,32 @@ import express from "express";
 import "dotenv/config";
 import "express-async-errors";
 import "./db";
-const cors = require("cors");
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+const allowedOrigins: string[] = [
+  "https://blogincs.vercel.app",
+  "http://localhost:3000",
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Origin is allowed
+    } else {
+      callback(new Error("Not allowed by CORS")); // Origin not allowed
+    }
+  },
+  credentials: true, // Include credentials such as cookies
+};
+
+app.use(cors(corsOptions));
 
 import authRouter from "./routes/auth";
 import blogRouter from "./routes/blog";
